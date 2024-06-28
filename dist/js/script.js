a9c4852d1f9e62e18f6544aae81ba9eae17c009c
josephines1 <josephine.bless777@gmail.com>
2024-06-28 13:21:22 +0700

    sticky navbar on mobile and tablet

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,10 +2,27 @@ const jsonPath = "assets/json/data.json";
 
 // Navbar
 const checkbox = document.querySelector(".nav-mobile input");
-const navMobile = document.querySelector(".nav-links-mobile");
+const navLinksMobile = document.querySelector(".nav-links-mobile");
+const navToggle = document.querySelector(".nav-mobile");
 
 checkbox.addEventListener("click", function () {
-  navMobile.classList.toggle("active");
+  navLinksMobile.classList.toggle("active");
+});
+
+document.addEventListener("scroll", function () {
+  const navbar = document.querySelector("nav");
+  if (window.scrollY === 0) {
+    navbar.classList.remove("scrolled");
+  } else {
+    navbar.classList.add("scrolled");
+  }
+});
+
+document.addEventListener("click", function (e) {
+  if (!navToggle.contains(e.target) && !navLinksMobile.contains(e.target)) {
+    navLinksMobile.classList.remove("active");
+    checkbox.checked = false;
+  }
 });
 
 // Projects
